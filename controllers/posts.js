7cc750dadc23c6b04a8fb56387f2ea89df18dc74
krystek77 <krystian.wrona77@gmail.com>
2021-11-05 11:04:53 +0100

    Add last 2  CRUD operations for posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -30,3 +30,30 @@ export const createPost = async (req,res) => {
         res.status(409).json({message:error.message});
     }
 }
+export const updatePost = async (req,res)=>{
+    const post = req.body;
+    const {id:_id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json("No post with that id");
+
+    try {
+        const updatedPost = await Post.findOneAndUpdate({_id},{...post,_id},{new:true});
+        res.status(200).json(updatedPost);
+        
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
+}
+
+export const deletePost = async (req,res)=>{
+    const {id:_id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json("No post with that id");
+    try {
+        await Post.findOneAndDelete({_id});
+        res.status(200).json("Post deleted sucessfully!");
+    } catch (error) {
+        res.status(409).json({message:error.message});
+    }
+
+
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getPosts,getPostById,createPost} from '../controllers/posts.js';
+import {getPosts,getPostById,createPost,updatePost,deletePost} from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/",getPosts);
 router.get("/:id",getPostById);
 router.post("/",createPost);
+router.patch("/:id",updatePost);
+router.delete("/:id",deletePost);
 
 export default router;
