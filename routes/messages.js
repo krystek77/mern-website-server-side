a54c8c658d27db14696412b7669855f3abaad088
krystek77 <krystian.wrona77@gmail.com>
2021-11-05 10:18:57 +0100

    Add route to create message

--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getMessages,getMessageById} from '../controllers/messages.js';
+import {getMessages,getMessageById,createMessage} from '../controllers/messages.js';
 
 const router = express.Router();
 
 
 router.get("/",getMessages);
 router.get("/:id",getMessageById);
+router.post("/",createMessage);
 
 export default router;
