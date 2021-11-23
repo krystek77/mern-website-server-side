810cce432b3b92dcb4c5e404e841c09b7525db66
krystek77 <krystian.wrona77@gmail.com>
2021-11-23 07:39:30 +0100

    Add the required field for the foreign key author

--- models/posts.js
+++ models/posts.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
   // creator: { type: String,trim:true, default: "some author" },
-  author:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
-  title: { type: String,trim:true, default: "some title" },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, trim: true, default: "some title" },
   contents: { type: String, default: "some contents" },
   selectedImage: String,
-  tags: [{type:String,trim:true,default:"some tag"}],
+  tags: [{ type: String, trim: true, default: "some tag" }],
   createdAt: {
     type: Date,
     default: new Date(),
