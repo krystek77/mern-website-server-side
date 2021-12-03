24d86e70e8da757c1bd73878b63919d2ba859351
krystek77 <krystian.wrona77@gmail.com>
2021-12-03 12:37:38 +0100

    Add another route to get posts by search

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,30 @@
-import Post from "../models/posts.js";
-import mongoose from "mongoose";
-import User from "../models/users.js";
+import Post from '../models/posts.js';
+import mongoose from 'mongoose';
+import User from '../models/users.js';
+
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query; //{searchQuery,tags}
+  const title = new RegExp(searchQuery, 'i');
+
+  try {
+    const posts = await Post.find(
+      {
+        $or: [{ title }, { tags: { $in: tags.split(',') } }],
+      },
+      '-__v'
+    ).populate({ path: 'author', select: 'firstName' });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
 
 export const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find({}, "-__v").populate({ path: "author", select: "firstName" });
+    const posts = await Post.find({}, '-__v').populate({
+      path: 'author',
+      select: 'firstName',
+    });
     // console.log(posts);
     return res.status(200).json(posts);
   } catch (error) {
@@ -13,10 +33,14 @@ export const getPosts = async (req, res) => {
 };
 export const getPostById = async (req, res) => {
   const { id: _id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: "No post with that ID" });
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).json({ message: 'No post with that ID' });
 
   try {
-    const post = await Post.findById(_id, "-__v").populate({ path: "author", select: "firstName" });
+    const post = await Post.findById(_id, '-__v').populate({
+      path: 'author',
+      select: 'firstName',
+    });
     return res.status(200).json(post);
   } catch (error) {
     return res.status(404).json({ message: error.message });
@@ -27,13 +51,18 @@ export const createPost = async (req, res) => {
 
   const { id } = req.user;
 
-  if (!id) return res.status(401).json({ message: "Unauthenticated" });
+  if (!id) return res.status(401).json({ message: 'Unauthenticated' });
 
   const newPost = new Post(post);
 
   try {
     const currentUser = await User.findById(id);
-    const resultPost = { ...post, _id: newPost._id, likes: [], author: { _id: currentUser._id, firstName: currentUser.firstName } };
+    const resultPost = {
+      ...post,
+      _id: newPost._id,
+      likes: [],
+      author: { _id: currentUser._id, firstName: currentUser.firstName },
+    };
     await newPost.save();
 
     return res.status(201).json(resultPost);
@@ -46,14 +75,19 @@ export const updatePost = async (req, res) => {
   const user = req.user;
   const { id: _id } = req.params;
 
-  if (!user.id) return res.status(401).json({ message: "Unauthenticated" });
+  if (!user.id) return res.status(401).json({ message: 'Unauthenticated' });
 
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: "No post with that ID" });
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).json({ message: 'No post with that ID' });
 
   try {
     const currentUser = await User.findById(user.id);
     const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
-    const returnedUpdatedPost = { ...post,_id: updatedPost._id, author: { _id: currentUser._id, firstName: currentUser.firstName } };
+    const returnedUpdatedPost = {
+      ...post,
+      _id: updatedPost._id,
+      author: { _id: currentUser._id, firstName: currentUser.firstName },
+    };
     return res.status(200).json(returnedUpdatedPost);
   } catch (error) {
     return res.status(409).json({ message: error.message });
@@ -62,10 +96,11 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
   const { id: _id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: "No post with that ID" });
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).json({ message: 'No post with that ID' });
   try {
     await Post.findByIdAndRemove(_id);
-    return res.status(200).json({ message: "Post deleted successfully" });
+    return res.status(200).json({ message: 'Post deleted successfully' });
   } catch (error) {
     return res.status(409).json({ message: error.message });
   }
@@ -74,12 +109,14 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
   const { id: _id } = req.params;
   const { id: userID } = req.user;
-  if (!userID) return res.status(401).json({ message: "Unauthorization" });
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.satatus(404).json({ message: "No post with that ID" });
+  if (!userID) return res.status(401).json({ message: 'Unauthorization' });
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.satatus(404).json({ message: 'No post with that ID' });
 
   try {
     const post = await Post.findById(_id);
-    if (post.author.toString() === userID) return res.status(400).json({ message: "You can not like your posts!" });
+    if (post.author.toString() === userID)
+      return res.status(400).json({ message: 'You can not like your posts!' });
     const index = post.likes.findIndex((id) => id.toString() === userID);
     if (index == -1) {
       //like
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,22 @@
-import express from "express";
-import { getPosts, getPostById, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
-import auth from "../middlewares/auth.js";
+import express from 'express';
+import {
+  getPosts,
+  getPostById,
+  getPostsBySearch,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from '../controllers/posts.js';
+import auth from '../middlewares/auth.js';
 const router = express.Router();
 
-router.get("/", getPosts);
-router.get("/:id", getPostById);
-router.post("/", auth, createPost);
-router.patch("/:id", auth, updatePost);
-router.delete("/:id", auth, deletePost);
-router.patch("/:id/likePost", auth, likePost);
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPostById);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
