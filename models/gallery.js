e19110bfc0c8e9f42a3819017d944917a5b51e6a
krystek77 <krystian.wrona77@gmail.com>
2021-12-13 12:15:58 +0100

    Remove fields rows and cols from photo model

--- models/gallery.js
+++ models/gallery.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 const photoSchema = new mongoose.Schema({
   title: { type: String, trim: true },
   image: { type: String },
-  rows: { type: Number },
-  cols: { type: Number },
   equipment: { type: String, trim: true },
   createdAt: { type: Date, default: Date.now() },
   lastUpdated: { type: Date, default: null },
