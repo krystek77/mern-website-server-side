c183cc66e16430f05549ce2843c48955857bc556
krystek77 <krystian.wrona77@gmail.com>
2021-12-14 10:15:42 +0100

    Modify photo model by adding default values and done at field

--- models/gallery.js
+++ models/gallery.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
 const photoSchema = new mongoose.Schema({
-  title: { type: String, trim: true },
-  image: { type: String },
-  equipment: { type: String, trim: true },
+  title: { type: String, trim: true, default: '' },
+  image: { type: String, default: '' },
+  equipment: { type: String, trim: true, default: '' },
   createdAt: { type: Date, default: Date.now() },
   lastUpdated: { type: Date, default: null },
+  doneAt: { type: Date, default: null },
 });
 const Photo = mongoose.model('Photo', photoSchema);
 export default Photo;
